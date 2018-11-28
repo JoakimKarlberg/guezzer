f7da5b104723296915133481d8ec08981e6e83bb
Joakim Karlberg <joakimkarlberg1@gmail.com>
2018-11-28 16:04:35 +0100

    added CategoryPage view + additional components
    components added: 
    - begin quiz button
    - quiz description
    - select category

--- guezzer.Client/src/App.vue
+++ guezzer.Client/src/App.vue
@@ -1,25 +1,39 @@
 <template>
   <div id="app">
-    <router-view/> 
-        <br>
-    <hr>
-    <a href="#/"> Go to Main page | </a><a href="#/YoutubePage">Try the Youtube Page</a>
+    <router-view/>
+    <div class="tempFooter">
+      <hr>
+      <p>
+      <a href="#/">Landing page</a> |
+      <a href="#/Resultpage">Result Page</a> |
+      <a href="#/Categorypage">Category Page</a>
+      <a href="#/YoutubePage">Try the Youtube Test Page</a>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.tempFooter{
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    height: 100px;
+    width: 100%;
+    overflow: hidden;
 }
 </style>
---
+++ guezzer.Client/src/components/BeginQuizButton.vue
@@ -0,0 +1,25 @@
+<template>
+     <div class="button-container">
+        <button class="btn btn-primary" v-on:click="BeginQuizButton">Begin</button>
+        <i class="fas fa-arrow-right"></i>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'BeginQuizButton',
+  methods: {
+    BeginQuizButton(category){
+    console.log('Begin Quiz with category ' + category)
+   }
+  }
+};
+</script>
+
+<style scoped>
+.button-container{
+  position: absolute;
+    top: 70%;
+    right: 20%;
+}
+</style>
