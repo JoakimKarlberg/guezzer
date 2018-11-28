26c2a834e5f1c3e7e925b14c8d1bea55de98929f
Simone Björklund <31948425+Simone1989@users.noreply.github.com>
2018-11-28 13:15:45 +0100

    Feature/landing page (#28)
    * Added ResultPage view component and Result component
    
    * Added components for PlayAgainButton, Homebutton and SubmitScore and imported them in ResultPage
    
    * Code now validating
    
    * Added bootstrap to result view and its components
    
    * Added toplist and startbutton components
    
    * Styled the page as the mockup and added a spot for the highscore list component
    
    * Changed routing for bootstrap
    
    * Fixed some css

---
+++ guezzer.Client/src/components/StartButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button class="startButton"> Start </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'StartButton',
+}
+</script>
+
+<style scoped>
+    .startButton{
+        background-color: #4CAF50;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        border-radius: 6px;
+        display: inline-block;
+        font-size: 30px;
+        -webkit-transition-duration: 0.4s; /* Safari */
+        transition-duration: 0.4s;
+    }
+    .startButton:hover {
+    background-color :#d01313;
+    }
+</style>
+
+d01313
+#4CAF50
+
+
--- guezzer.Client/src/router/index.js
+++ guezzer.Client/src/router/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import VueYoutube from 'vue-youtube'
 import Router from 'vue-router'
-import MainPage from '@/views/MainPage'
+import LandingPage from '@/views/LandingPage'
 import YoutubePage from '@/views/YoutubePage'
+import ResultPage from '@/views/ResultPage'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
 
 Vue.use(Router)
 Vue.use(VueYoutube)
+Vue.use(LandingPage)
 
 // Here we create the routes
 // First you need to import the component, then specify the name of the route and which component to bind it to
@@ -13,13 +18,19 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'MainPage',
-      component: MainPage
+      name: 'LandingPage',
+      component: LandingPage
     },
     {
       path: '/YoutubePage',
       name: 'YoutubePage',
       component: YoutubePage
+    },
+    {
+      path: '/ResultPage',
+      name: 'ResultPage',
+      component: ResultPage
     }
+
   ]
 })
