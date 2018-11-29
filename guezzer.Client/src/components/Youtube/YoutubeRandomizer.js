3c313064c89a0cc4f71409eab9c7a1f22e406c2c
Molyn Eriksson <molyneriksson@gmail.com>
2018-11-29 10:26:14 +0100

    Clean up

--- guezzer.Client/src/components/Youtube/YoutubeRandomizer.js
+++ guezzer.Client/src/components/Youtube/YoutubeRandomizer.js
@@ -1,10 +1,11 @@
-export default {GetRandomCategory}
-
-function GetRandomCategory() {
-
-     let categoriesList = ["all","music","funny"]
-     
-     var randomCategory = categoriesList[Math.floor(Math.random() * categoriesList.length)];
-
-     return randomCategory;     
-    }
+export default {
+     data:{ 
+          categoryList: ["all","music","funny","cats","epic fails"],
+          orderList: ["date","rating","relevance","title","videoCount","viewCount"]
+     }
+     ,
+     methods:{
+          GetRandomValue:function(array){
+          return array[Math.floor(Math.random() * array.length)];;
+     }}
+}
