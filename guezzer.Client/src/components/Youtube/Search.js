240d0fb54f393a2c4a0c1261fd8e6943cc4bb13c
Molyn Eriksson <molyneriksson@gmail.com>
2018-11-29 14:15:11 +0100

    Revert "bug fix again"
    This reverts commit f06176cce515daef8c5f9812a5f53fef5bea0219.

--- guezzer.Client/src/components/Youtube/Search.js
+++ guezzer.Client/src/components/Youtube/Search.js
@@ -14,10 +14,10 @@ module.exports = function(options, callback) {
     let params = {
         part: 'snippet',
         key: options.apiKey,
-        order: options.order,
         q: options.searchWord,
         maxResults: (options.maxResults) ? options.maxResults : 1,
-        type: 'video', 
+        type: 'video',
+        sortOrder: options.sortOrder
     };
 
     axios.get(Base_URL, {params})
