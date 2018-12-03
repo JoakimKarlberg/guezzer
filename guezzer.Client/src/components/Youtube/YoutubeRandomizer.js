c31fd7e1663fcbbc7bc314f0bd61dbf83a1d6d14
Molyn Eriksson <molyneriksson@gmail.com>
2018-12-03 11:37:41 +0100

    Added musiccategorylist

--- guezzer.Client/src/components/Youtube/YoutubeRandomizer.js
+++ guezzer.Client/src/components/Youtube/YoutubeRandomizer.js
@@ -1,10 +1,13 @@
 export default {
      data:{ 
-          categoryList: ["all","music","funny","cats","epic fails","gaming",
+          randomCategoryList: ["all","music","funny","cats","epic fails","gaming",
           "sport","tv","dance","cartoon","travel","irl","programming","weather",
           "christmas", "rick and morty", "animals","cats", "dogs","fails","puppies"],
           
-          orderList: ["date","rating","relevance","title","videoCount","viewCount"]
+          orderList: ["date","rating","relevance","title","videoCount","viewCount"],
+
+          musicCategoryList: ["metal","classical","reggae", "jazz", "house", "pop", "hard rock", "R&B",
+          "singer/songwriter","opera","asian pop","electronic music","christmas songs"]
      }
      ,
      methods:{
