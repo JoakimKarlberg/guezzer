5b667b3541e03952f3e59f970df96198497fcd31
Molyn Eriksson <31955751+molyne@users.noreply.github.com>
2018-12-03 13:23:17 +0100

    Feature/youtube randomizer categories (#48)
    * Added musiccategorylist
    
    * randomize different categories functionality
    
    * cleanup

--- guezzer.Client/src/components/Youtube/VideoItem.vue
+++ guezzer.Client/src/components/Youtube/VideoItem.vue
@@ -37,8 +37,8 @@ export default {
       // This is a request to the youtube-API and basically gets the youtube ID that is used in the ':video-id="fetchedVideoId"' binding in the HTML.
       Search({
             apiKey: YoutubeApiKey, 
-            searchWord: YoutubeRandomizer.methods.GetRandomValue(YoutubeRandomizer.data.categoryList), // This little cutie simulates searching 'cats' on youtube and picking first video - should be randomized
-            sortOrder: YoutubeRandomizer.methods.GetRandomValue(YoutubeRandomizer.data.orderList)
+            searchWord: YoutubeRandomizer.methods.GetSelectedCategory("funny"), // This little cutie simulates searching 'cats' on youtube and picking first video - should be randomized
+            sortOrder: YoutubeRandomizer.methods.GetRandomOrder()
         }, response => this.fetchedVideoId = response.id.videoId); // should not return only Id. Should return the whole response instead so you can get the data you want from the youtube request.
     }
 }
--- guezzer.Client/src/components/Youtube/YoutubeRandomizer.js
+++ guezzer.Client/src/components/Youtube/YoutubeRandomizer.js
@@ -1,14 +1,38 @@
 export default {
-     data:{ 
-          categoryList: ["all","music","funny","cats","epic fails","gaming",
-          "sport","tv","dance","cartoon","travel","irl","programming","weather",
-          "christmas", "rick and morty", "animals","cats", "dogs","fails","puppies"],
-          
-          orderList: ["date","rating","relevance","title","videoCount","viewCount"]
-     }
-     ,
      methods:{
-          GetRandomValue:function(array){
-          return array[Math.floor(Math.random() * array.length)];;
-     }}
+          GetSelectedCategory: function(selectedCategory){
+               switch (selectedCategory) {
+                    case "music":
+                         return GetRandomValue(musicCategoryList);    
+
+                    case "random":
+                         return GetRandomValue(randomCategoryList);
+
+                    case "funny":
+                         return GetRandomValue(funnyCategoryList);
+                    default:
+                         break;
+               }
+          },
+          GetRandomOrder: function(){
+               let orderList = ["date","rating","relevance","title","videoCount","viewCount"];
+               return GetRandomValue(orderList);
+          }      
+     }
 }
+
+function GetRandomValue(array){
+     return array[Math.floor(Math.random() * array.length)];;
+}
+
+let randomCategoryList = ["all","music","funny","cats","epic fails","gaming",
+"sport","tv","dance","cartoon","travel","irl","programming","weather",
+"christmas", "rick and morty", "animals","cats", "dogs","fails","puppies"];
+
+let musicCategoryList = ["music metal","music classical","music reggae", "music jazz", "music house", 
+"music pop", "music hard rock", "music R&B",
+"music singer/songwriter","music opera","music asian pop","electronic music","music christmas songs"];
+
+let funnyCategoryList = ["funny fails", "funny animals", "funny clowns", "funny pranks", "funny moments", "funny videos",
+"funny laugh", "funny songs", "funny babies", "funny languages", "funny drunk people", "funny people",
+"funny news fail"];
