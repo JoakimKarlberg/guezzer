dfa7ff0eb0ffca69eac003b90aaa69b2e10a8bce
Simone Björklund <simone_1000@hotmail.com>
2018-12-07 09:55:47 +0100

    Installed vuetify

--- guezzer.Client/src/router/index.js
+++ guezzer.Client/src/router/index.js
@@ -5,10 +5,13 @@ import Router from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Router)
 Vue.use(VueYoutube)
-Vue.use(BootstrapVue);
+Vue.use(BootstrapVue); 
+Vue.use(Vuetify)
 
 // Import pages
 import LandingPage from '@/views/LandingPage'
