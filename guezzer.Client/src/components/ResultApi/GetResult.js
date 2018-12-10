8b71c71a730e0d9a3b92fc9b241c9240bad20337
Johanna <johannaastrom@live.se>
2018-12-10 14:45:06 +0100

    Table with data populated for TopList

--- guezzer.Client/src/components/ResultApi/GetResult.js
+++ guezzer.Client/src/components/ResultApi/GetResult.js
@@ -1,15 +1,44 @@
-const axios = require('axios');
+import axios from 'axios'
 const url = 'http://localhost:5000/api/Results';
 
-module.exports = function(playerName, category, score){
-       axios({
-         method: 'get',
-         url: url,
-         data: {
-           Name: playerName,
-           Category: category,
-           Score: score
-         }
-       })
-       .then(results => console.log(results));
+export default {
+  name: 'GetResult',
+  data () {
+    return {
+      results: {}
+    }
+  },
+  created () {
+    axios.get(url)
+      .then(response => {
+        console.log(response.data)
+        this.results = response.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 }
+
+
+
+
+
+// const axios = require('axios');
+// const url = 'http://localhost:5000/api/Results';
+
+// module.exports = function(){
+//        axios({
+//          method: 'get',
+//          url: url,
+//          data() {
+//             return {
+//                 results: []
+//             }
+//          }
+//        })
+//        .then(response => {
+//          this.results = response.data
+//          console.log(this.results)
+//         });
+// }
--- guezzer.Client/src/components/TopList.vue
+++ guezzer.Client/src/components/TopList.vue
@@ -1,57 +1,73 @@
 <template>
-    <div class="list">
-        <h3>{{ header }} {{ category }}</h3>
-        <label for="score">{{score}}</label>
-        <ul>
-            <li>{{ score }}</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <GetResult></GetResult>
-        </ul>
+    <div class="TopList">
+        <h3>{{ header }}</h3>
+     <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Score</th>
+          <th>category</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="result in results" :key="result.id">
+          <td>{{ result.name }}</td>
+          <td>{{ result.score }}</td>
+          <td>{{ result.category }}</td>
+        </tr>
+      </tbody>
+    </table>
     </div>
 </template>
 
+// <script>
+// import axios from 'axios'
+// import GetResult from './ResultApi/GetResult.js'
+
+// export default {
+//     name: 'TopList',
+//     data() {
+//     return {
+//        header: "Top score for category: ",
+//        results: []
+//       }
+//     },
+//     methods: {
+//     getResult: function() {//getResult function should be called in template? make a list from this??
+//       GetResult()
+//     }
+//   }
+// }
+// </script>
+
 <script>
-import GetResult from './ResultApi/GetResult.js'
+import axios from 'axios'
+const url = 'http://localhost:5000/api/Results';
 
 export default {
-    name: 'TopList',
-    components: { //here comes the response from GetResult.js
-        GetResult
-    },
-    data() {
+  name: 'TopList',
+  data () {
     return {
-        //properties defined here
-      header: "Top score for category: ",
-      category: "",
-      playerName: "",
-      score: "",
-      topList: [] // playerName + score 
-      };
-    },
-    methods: {
-    getResult: function() {
-        console.log('JALLLLAAA' + this.score);
-      GetResult(this.playerName, this.category, this.score)
+      results: {},
+      header: 'Toplist for category'
     }
+  },
+  created () {
+    axios.get(url)
+      .then(response => {
+        console.log(response.data)
+        this.results = response.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
 </script>
 
+
 <style scoped>
-    li{
-        list-style-type: none;
-    }  
-    ul{
-        display:inline-block;
-        border:3px solid #000;
-        padding:20px;
-    }
-    .list{
-        padding: 1%;
-    }
+
 </style>
 
 
